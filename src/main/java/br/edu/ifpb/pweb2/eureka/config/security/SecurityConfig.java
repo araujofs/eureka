5294d05cd6a9cee22dbf1b9173725b21408b07f0
Arthur <arthurfelipe5567@gmail.com>
2026-06-15 19:10:28 -0300

    feat: set spring to use my login form
    closes #26

---
+++ src/main/java/br/edu/ifpb/pweb2/eureka/config/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package br.edu.ifpb.pweb2.eureka.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    http
+        .csrf(csrf -> csrf.disable())
+        .formLogin(form -> form
+            .loginPage("/auth")
+            .loginProcessingUrl("/auth")
+            .usernameParameter("name")
+            .defaultSuccessUrl("/home")
+            .failureUrl("/auth")
+            .permitAll())
+        .logout(logout -> logout
+            .logoutUrl("/auth/logout"));
+
+    return http.build();
+  }
+
+  @Bean
+  AuthenticationManager authenticationManager(UserDetailsService service, PasswordEncoder encoder) {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(service);
+    authProvider.setPasswordEncoder(encoder);
+
+    return new ProviderManager(authProvider);
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+}

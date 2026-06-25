ac6a59f6c385459cc68013fdae330f923c33566f
Arthur <arthurfelipe5567@gmail.com>
2026-06-25 10:05:52 -0300

    feat(ranking): paginate ranking queries

--- src/main/java/br/edu/ifpb/pweb2/eureka/result/ResultRepository.java
+++ src/main/java/br/edu/ifpb/pweb2/eureka/result/ResultRepository.java
@@ -2,11 +2,16 @@ package br.edu.ifpb.pweb2.eureka.result;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.ifpb.pweb2.eureka.race.Race;
 import br.edu.ifpb.pweb2.eureka.user.User;
 
+
 public interface ResultRepository extends JpaRepository<Result, Long> {
   Optional<Result> findByParticipantAndRace(User participant, Race race);
+
+  Page<Result> findByRace(Race race, Pageable pageable);
 }
